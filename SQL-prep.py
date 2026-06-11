0a4c3963d25fe35ceb83a16995f4ea716554ca87
Bunty Saini <sainibunty2309@gmail.com>
2026-06-11 21:34:40 +0530

    ass sql solution

--- SQL-prep.py
+++ SQL-prep.py
@@ -255,3 +255,27 @@ where datediff(w1.recordDate, w2.recordDate)= 1
 and w1.temperature > w2.temperature;
 
 '''
+
+# 1661. Average time of process per machine
+# Link -> https://leetcode.com/problems/average-time-of-process-per-machine/description/?envType=study-plan-v2&envId=top-sql-50
+
+'''
+There is a factory website that has several machines each running the same
+number of processes. Write a solution to find the average time each 
+machine takes to complete a process.
+
+The time to complete a process is the 'end' timestamp minus the
+'start' timestamp. The average time is calculated by the total time 
+to complete every process on the machine divided by the number of 
+processes that were run.
+'''
+# Solution:
+
+'''
+select a.machine_id, round(avg(b.timestamp - a.timestamp),3) as processing_time
+from Activity a
+join Activity b
+on a.machine_id = b. machine_id and a.process_id = b.process_id
+where a.activity_type = 'start' and b.activity_type = 'end'
+group by a.machine_id;
+'''
