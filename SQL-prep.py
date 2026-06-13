ffb4ca3c6b0b92037edcb945d4b1087cc4f088b5
Bunty Saini <sainibunty2309@gmail.com>
2026-06-13 21:04:44 +0530

    add sql solution

--- SQL-prep.py
+++ SQL-prep.py
@@ -403,3 +403,23 @@ select class from Courses
 group by class
 having count(student)>= 5
 '''
+
+#Monthly Transactions I
+# Link ->https://leetcode.com/problems/monthly-transactions-i/description/?envType=problem-list-v2&envId=db-db2-filtering-aggregation
+
+'''
+Write an SQL query to find for each month and country, the number of transactions and their total amount, 
+the number of approved transactions and their total amount.
+'''
+
+# Solution :
+
+'''
+select DATE_FORMAT(trans_date,'%Y-%m') as month,country,
+     count(*) as trans_count,
+     sum(case when state = 'approved' then 1 else 0 END ) as approved_count,
+     sum(amount) as trans_total_amount, 
+     sum(case when state = 'approved' then amount else 0 end) as approved_total_amount 
+     from Transactions
+     group by DATE_FORMAT(trans_date,'%Y-%m'), country
+'''
