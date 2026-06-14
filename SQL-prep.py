ae72bf7909be8c73bed7665bd808337b2f8a41e5
Bunty Saini <sainibunty2309@gmail.com>
2026-06-14 22:06:56 +0530

    add 585

--- SQL-prep.py
+++ SQL-prep.py
@@ -584,3 +584,33 @@ FROM (
 WHERE num = prev1
   AND num = prev2;
 '''
+
+# Investments in 2016
+# Link ->https://leetcode.com/problems/investments-in-2016/description/?envType=problem-list-v2&envId=db-db5-sql-ii
+
+'''
+Write a solution to report the sum of all total investment values in 
+2016 tiv_2016, for all policyholders who:
+
+- have the same tiv_2015 value as one or more other policyholders, and
+- are not located in the same city as any other policyholder 
+(i.e., the (lat, lon) attribute pairs must be unique).
+Round tiv_2016 to two decimal places.
+'''
+# 
+'''SELECT
+    ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+        SELECT tiv_2015
+        FROM Insurance
+        GROUP BY tiv_2015
+        HAVING COUNT(*) > 1
+      )
+  AND (lat, lon) IN (
+        SELECT lat, lon
+        FROM Insurance
+        GROUP BY lat, lon
+        HAVING COUNT(*) = 1
+      );
+'''
