2e539ce44f7ae244e01f82fb432145323d8be767
Bunty Saini <sainibunty2309@gmail.com>
2026-06-15 22:14:29 +0530

    1929 dsa

--- leetcode.py
+++ leetcode.py
@@ -61,3 +61,22 @@
         return ans
 """
 
+# 1929. Concatenation of Array
+# Link -> https://leetcode.com/problems/concatenation-of-array/submissions/2034185418/?envType=problem-list-v2&envId=dsa-linear-shoal-array-i
+
+'''
+Given an integer array nums of length n, you want to create an array ans 
+of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] 
+for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+'''
+
+# Solution :
+
+'''
+class Solution(object):
+    def getConcatenation(self, nums):
+        return nums + nums
+'''
