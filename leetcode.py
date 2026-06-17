2d4cb970434ee74f6c7f300006d2a4830cc0332d
Bunty Saini <sainibunty2309@gmail.com>
2026-06-17 19:18:45 +0530

    167 array

--- leetcode.py
+++ leetcode.py
@@ -389,3 +389,38 @@ class Solution(object):
 
         return ans       
 '''
+
+# 167,. Two Sum II (Array)
+# Link -> https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+'''
+Given a 1-indexed array of integers numbers that is already sorted in 
+non-decreasing order, find two numbers such that they add up to a 
+specific target number. Let these two numbers be numbers[index1] 
+and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers index1 and index2, each 
+incremented by one, as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. 
+You may not use the same element twice.
+'''
+
+# Solution :
+
+'''
+class Solution(object):
+    def twoSum(self, numbers, target):
+        left = 0
+        right = len(numbers)-1
+
+        while left < right:
+            s = numbers[left] + numbers[right]
+
+            if s == target:
+                return [left +1, right+1]
+            elif s < target:
+                left += 1
+            else:
+                right -=1
+'''
